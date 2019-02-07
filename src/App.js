0ce000d47a9d4ed0cbaaccb81101830b9030a334
Norbert Savareika <norbert.savareika@gmail.com>
2019-02-07 22:46:07 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import useWindowWidth from './hooks/useWindowWidth';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default () => {
+  const windowWidth = useWindowWidth();
+  return (
+    <div>
+      <h4>Window size: {windowWidth}px</h4>
+      {windowWidth < 768 ? (
+        <div>Mobile resolution</div>
+      ) : (
+        <div>Desktop resolution</div>
+      )}
+    </div>
+  );
+};
